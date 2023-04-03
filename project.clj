db9ee5fa958ec448af0d473bb1e90034f032eadf
dotan <dotan@cycognito.com>
2023-04-03 13:47:49 +0300

    Ready project.clj for Clojars

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject baby-sepl "1.0.1"
-  :description "Baby version of the SEPL algorithm execution engine"
-  :url "http://example.com/FIXME"
+(defproject com.github.s-doti/baby-sepl "1.0.1"
+  :description "Baby version of the SEPL (algorithm execution) engine"
+  :url "https://github.com/s-doti/baby-sepl"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
